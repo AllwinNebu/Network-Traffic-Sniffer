b4680f91df15ff83a21fc062a1be26e602048e26
AllwinNebu <86886342+AllwinNebu@users.noreply.github.com>
2025-05-21 20:10:29 +0530

    Mac Address readable

--- Sniffer.py
+++ Sniffer.py
@@ -6,3 +6,8 @@ import textwrap
 def ethernet_frame(data):
     dest_mac , src_mac , proto = struct.unpack('! 6s 6s H',data[:14])
     return get_mac_addr (dest_mac), get_mac_addr(src_mac), socket.htonl(proto), data[:14]
+
+#convert mac adddress we get into readable format
+def get_mac_addr(bytes_addr):
+    byte_str = map('{:02x}'.format , bytes_addr)
+    return ':'.join(byte_str).upper()
